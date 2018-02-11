d703eb8f429619d732ac90988e1280c9354453f4
kothadiaab <kothadiaab@gmail.com>
2018-02-11 16:06:04 -0500

    Webpack configarationchanges and HTML CSS loaders

---
+++ src/js/app.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../css/app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello Prachi Kothadia</h1>
+    );
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,53 @@
-const path = require("path");
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebPackPlugin = require('Html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'css/style.css'
+});
 
 module.exports = {
-  entry: ["./src/js/index.jsx"],
+  entry: ['./src/js/index.jsx'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/build.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/build.js'
   },
+  devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: "./dist"
+    contentBase: './dist'
   },
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
       }
     ]
-  }
+  },
+  plugins: [
+    new UglifyJSPlugin({sourceMap: true}),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new CleanWebpackPlugin(['dist']),
+    extractPlugin
+  ]
 };
