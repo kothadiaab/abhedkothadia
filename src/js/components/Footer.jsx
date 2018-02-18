9d2a5737c77a3353d5a2c2cb8b803dfe9504aeef
kothadiaab <kothadiaab@gmail.com>
2018-02-18 18:18:41 -0500

    variables for css and header implimentation

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Footer extends Component {
   render() {
     return (
-      <div>
+      <div className="footer">
         <h3>Inside footer</h3>
         <ul>
           <li>
--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -4,29 +4,23 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>Abhed Kothadia</h1>
-        <ul>
-          <li>
-            <Link to="/">Profile</Link>
-          </li>
-          <li>
-            <Link to="/skills">Skills</Link>
-          </li>
-          <li>
-            <Link to="/work">Work</Link>
-          </li>
-          <li>
-            <Link to="/education">Education</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
+      <header className="header">
+        <div className="container">
+          <div className="header-title">
+            <h1><Link to="/">Abhed Kothadia</Link></h1>
+          </div>
+          <div className="header-navigation">
+            <ul>
+              <li><Link to="/">Profile</Link></li>
+              <li><Link to="/skills">Skills</Link></li>
+              <li><Link to="/work">Work</Link></li>
+              <li><Link to="/education">Education</Link></li>
+              <li><Link to="/blogs">Blogs</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </div>
+        </div>
+      </header>
     );
   }
 }
